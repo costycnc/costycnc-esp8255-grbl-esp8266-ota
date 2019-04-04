53e5fd210791622fdcd64dc9f4dea15a3c6687e8
Boboaca Costel <bobyca2003@yahoo.com>
2019-04-04 14:36:38 +0200

    Add files via upload

---
+++ change_mac_address.ino
@@ -0,0 +1,24 @@
+// https://yoursunny.com/t/2017/change-ESP8266-MAC/
+
+//mac return at first value when reset
+
+#include <ESP8266WiFi.h>
+#include <Arduino.h>
+extern "C" {
+#include <user_interface.h>
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+
+  Serial.print("MAC address is ");
+  Serial.println(WiFi.macAddress());
+uint8_t mac[6] {0xb8, 0xd7, 0x63, 0x00, 0xfe, 0xef};
+wifi_set_macaddr(STATION_IF, mac);
+  Serial.print("MAC address is ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+}
